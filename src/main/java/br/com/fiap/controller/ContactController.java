8e4a4c80da996b2d99e8e033d9e421681fe963aa
luizkt <keese.luiz@gmail.com>
2020-03-28 12:25:21 -0300

    Contact Controller fix

--- src/main/java/br/com/fiap/controller/ContactController.java
+++ src/main/java/br/com/fiap/controller/ContactController.java
@@ -39,18 +39,8 @@ public class ContactController {
             @ApiResponse(code = 400, message = "Some field have wrong information"),
             @ApiResponse(code = 500, message = "Some error occurred"),
     })
-<<<<<<< Updated upstream:src/main/java/br/com/fiap/controller/TransactionController.java
-<<<<<<< HEAD
-    public ResponseEntity<List<Contact>> findAllTransactionsFromStudent(@PathVariable Integer studentRegistrationNumber) {
-        return contactService.findAllTransactionsFromStudent(studentRegistrationNumber);
-=======
-    public ResponseEntity<List<Contact>> findAllTransactionsFromCollaborator(@PathVariable Integer CollaboratorRegistrationNumber) {
-        return transactionService.findAllTransactionsFromCollaborator(CollaboratorRegistrationNumber);
->>>>>>> 8d0c044dbaf1bf7239cfa69b92347bd0f054c5d5
-=======
     public ResponseEntity<List<Contact>> findAllContactsFromCollaborator(@PathVariable("collaboratorRegistrationNumber") Integer collaboratorRegistrationNumber) {
         return contactService.findAllContactsFromCollaborator(collaboratorRegistrationNumber);
->>>>>>> Stashed changes:src/main/java/br/com/fiap/controller/ContactController.java
     }
 
     @RequestMapping(path = "/{contactId}", method = RequestMethod.DELETE, produces="application/json")
@@ -61,12 +51,7 @@ public class ContactController {
             @ApiResponse(code = 400, message = "Some field have wrong information"),
             @ApiResponse(code = 500, message = "Some error occurred"),
     })
-<<<<<<< Updated upstream:src/main/java/br/com/fiap/controller/TransactionController.java
-    public ResponseEntity<String> deleteTransactionById(@PathVariable Integer transactionId) {
-        return contactService.deleteTransactionById(transactionId);
-=======
     public ResponseEntity<String> deleteTransactionById(@PathVariable("contactId") Integer contactId) {
         return contactService.deleteTransactionById(contactId);
->>>>>>> Stashed changes:src/main/java/br/com/fiap/controller/ContactController.java
     }
 }
